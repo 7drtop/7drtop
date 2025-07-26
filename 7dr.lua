0a1b3ede70c166f31cf6beabf7807be407049838
7dr <maxontop99@gmail.com>
2025-07-26 04:30:04 +0300

    Update 7dr.lua

--- 7dr.lua
+++ 7dr.lua
@@ -1,9 +1,10 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-wait(1)
-local gui = game:GetService("CoreGui"):FindFirstChild("KavoUI")
-if gui then
-    local main = gui:FindFirstChild("Main")
-    if main then
-        main.Size = UDim2.new(60, 60, 50, 50) -- غيّر الحجم حسب رغبتك
+Section:NewToggle("Teleport Up Once", "ينقلك لفوق مرة وحدة", function(state)
+    if state then
+        local player = game.Players.LocalPlayer
+        local char = player.Character
+        if char and char:FindFirstChild("HumanoidRootPart") then
+            char.HumanoidRootPart.CFrame = char.HumanoidRootPart.CFrame + Vector3.new(0, 10, 0)
+        end
     end
-end
+end)
