26932f42f8fdb3f91a209ccf65ab55c2c9f3cdc7
7dr <maxontop99@gmail.com>
2025-07-26 04:25:10 +0300

    Update 7dr.lua

--- 7dr.lua
+++ 7dr.lua
@@ -4,6 +4,6 @@ local gui = game:GetService("CoreGui"):FindFirstChild("KavoUI")
 if gui then
     local main = gui:FindFirstChild("Main")
     if main then
-        main.Size = UDim2.new(0, 60, 0, 50) -- غيّر الحجم حسب رغبتك
+        main.Size = UDim2.new(60, 60, 50, 50) -- غيّر الحجم حسب رغبتك
     end
 end
